ad67e30cf8030339670712d46cb564323ea5936d
Esranurr46 <ecobanlar90@gmail.com>
2025-10-23 09:10:39 +0300

    22.10.2025

--- App.js
+++ App.js
@@ -1,47 +1,35 @@
+import react from "react";
+import { StyleSheet, Text, View, StatusBar, ActivityIndicator, Button } from "react-native"; 
+import Greet from "./src/components/Greet.js";
 
-import { StatusBar } from 'expo-status-bar'; 
-import { StyleSheet, Text, View , Button, TouchableOpacity, TouchableHighlight , TouchableWithoutFeedback} from 'react-native'; 
-import Title from './src/components/title'; 
-import Header from './src/components/Header';
- import Asd from './src/components/Asd';
- import User from './src/components/User';
-  import Users from './src/components/Users'; 
-  const App= ({}) => { 
-    const fonk=() => {
-      alert('hay')
+
+const App=()=> {
+    return(
+
+
+
+        <View style={styles.container}>
+
+        {/*<Button title="Alert" onPress={() => alert('Button Pressed!')} />
+           <Text>aös</Text>
+           <StatusBar barStyle="light-content" backgroundColor="lightreen" hidden/>
+<ActivityIndicator size="small" color="blue" hidden/>*/}
+<Greet name="John Doe"/>
+<Text> StyleSheet Api</Text>
+
+
+
+
+           
+      </View>
+    )
+}
+
+const styles=StyleSheet.create({
+    container:{
+        flex:1,
+        padding:60,
+        backgroundColor:'plum',
     }
-    return (
-       <View style={styles.container}> 
-       {/* <Title  text= 'esra' color={'green'} bool={true}/> 
-    {/* <User data={{id:1, name:'esra'}} />
-     < Users data={['esra', 'esra']} />
-     <Header/> <Asd/>
-       <StatusBar style="auto" /> 
-        */}
-
-
-<Button title="Press me" onPress={(fonk)} />  
-<TouchableOpacity onPress={(fonk)}>
-<Text>Press you</Text>
-</TouchableOpacity>
-<TouchableHighlight
-  activeOpacity={0.3}
-  underlayColor="red"
-  onPress={() => alert('Pressed!')}>
-<Text>ABC</Text>
-
-</TouchableHighlight>
-<TouchableWithoutFeedback onPress={() => alert('hopp!')}>
-<Text>ABCdf</Text>
-</TouchableWithoutFeedback>
-
-       </View> ); }
-        const styles = StyleSheet.create({ 
- container: 
- { flex: 1,
-  backgroundColor: '#fff',
-  alignItems: 'center',
-  justifyContent: 'center',   },
-  });
-
- export default App;
+})
+export default App;
